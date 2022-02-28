cbc90ced164ab2b8d8e95f2874dfb48be8a6fa0c
David Matthew Lopez <dmatthew2005@gmail.com>
2022-02-28 08:17:31 -0500

    Testing Testing

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -11,7 +11,7 @@
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('calendar') }}" :active="request()->routeIs('Calendar')">
                         {{ __('Calendar') }}
                     </x-jet-nav-link>
                 </div>
