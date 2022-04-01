a7cd11a103649a97d76b85fdbba930aae5644f28
David Lopez <dlopez@mattmen.com>
2022-03-31 23:15:40 -0400

    User Permissions. I ran a query on the users table using the isAdmin column. The isAdmin column is a boolean which will have a value of 1 or 0.  1 = true, 0 = false. If the current user is an Admin, the column isAdmin will have a value of 1 which equals true. True value will dislay the Add Blog button. If the current  user is not an Admin, the column isAdmin will have a value of 0 which equals false.  False value will not display the Add Blog Button.

--- app/Http/Livewire/BlogHeader.php
+++ app/Http/Livewire/BlogHeader.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class BlogHeader extends Component
 {
     public function render()
     {
-        return view('livewire.blog-header');
+        return view('livewire.blog-header', [
+            'isAdmin' => auth()->user()->isAdmin
+        ]);
     }
 }
--- resources/views/livewire/blog-header.blade.php
+++ resources/views/livewire/blog-header.blade.php
@@ -10,21 +10,28 @@
             Blog List
         </h1>
 
-        <!-- Right side container:
-        ex: Today
-        ex: Monthly View
-        ex: Add Event button
-        -->
         <div class="flex items-center">
 
-            <!-- Calendar Dropdown view Menu for medium to larger screens. -->
-            <div class="hidden md:ml-4 md:flex md:items-center">
+                <!-- Dropdown view Menu for medium to larger screens. -->
+                
+                    @if ($isAdmin)
+                        <h1>ADMIN USER</h1>
 
-                <div class="ml-6 h-6 w-px bg-gray-300"></div>
-                <button wire:click="$emit('showBlogModal')" type="button" class="focus:outline-none ml-6 rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add Blog</button>
-            </div>
+                        <div class="hidden md:ml-4 md:flex md:items-center">
 
-            <!-- Calendar Dropdown view Menu for small screens. -->
+                            <div class="ml-6 h-6 w-px bg-gray-300"></div>
+                            <button wire:click="$emit('showBlogModal')" type="button" class="focus:outline-none ml-6 rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add Blog</button>
+                        </div>
+                        
+                    @else
+                        <h1>NOT ADMIN</h1>
+                    @endif
+             
+                
+
+            
+
+            <!-- Dropdown view Menu for small screens. -->
             <div class="relative ml-6 md:hidden">
                 <button @click="open = ! open" type="button" class="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500" id="menu-0-button" aria-expanded="false" aria-haspopup="true">
                     <span class="sr-only">Open menu</span>
