3a2c27ec2ba9ef850e21010f2046684347ec1708
David Lopez <dlopez@mattmen.com>
2022-04-12 13:33:40 -0400

    Blog Update to Dashboard. I added the Blog update component to the Dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,7 @@
                 @livewire('blog-view')
                 @livewire('blog-create')
                 @livewire('blog-details')
+                @livewire('blog-update')
 
             </div>
         </div>
