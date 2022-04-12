663b54e2c18cf0efef1f33ff2dfad48d7e8e928d
David Lopez <dlopez@mattmen.com>
2022-04-12 11:01:03 -0400

    Modal Updates Removed date field

--- resources/views/livewire/blog-create.blade.php
+++ resources/views/livewire/blog-create.blade.php
@@ -26,21 +26,6 @@
                         @enderror
                     </div>
 
-                    <!-- Date Field -->
-                    <div class="w-full px-5 mb-6">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            for="grid-date">
-                            Confirm Current Date: {{ date('Y-m-d') }}
-                        </label>
-                        <input wire:model.lazy="date"
-                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-date" name="grid-date" type="checkbox"
-                            required>
-                        @error('date')
-                            <span class="error text-red-700 font-bold">{{ $message }}</span>
-                        @enderror
-                    </div>
-
                     <!-- Text Field -->
                     <div class="w-full px-5 mb-6">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
